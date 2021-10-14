6b0b8893ba7cfeea30754583639f2fbdd1ae97a2
kmaslennikov <82436599+kmaslennikov@users.noreply.github.com>
2021-10-14 10:37:20 +0000

    Const + add limit conditions

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	minScopes := 275
+	const minScopes = 275
+	const limit = 100
 
 	var scopes1 int
 	var scopes2 int
@@ -14,21 +15,38 @@ func main() {
 	fmt.Println("Введите результат первого экзамена:")
 	fmt.Scan(&scopes1)
 
-	fmt.Println("Введите результат второго экзамена:")
-	fmt.Scan(&scopes2)
-
-	fmt.Println("Введите результат третьего экзамена:")
-	fmt.Scan(&scopes3)
-
-	fmt.Println("Сумма проходных баллов:", minScopes)
-
-	totalScopes := scopes1 + scopes2 + scopes3
-	fmt.Println("Количество набранных баллов:", totalScopes)
-
-	if totalScopes >= minScopes {
-		fmt.Println("Поздравляем! Вы поступили в институт.")
+	if scopes1 < 0 {
+		fmt.Println("Значение баллов не может быть отрицательным")
+	} else if scopes1 > limit {
+		fmt.Println("Значение баллов не может превышать", limit, "баллов")
 	} else {
-		fmt.Println("Вы не поступили.")
+		fmt.Println("Введите результат второго экзамена:")
+		fmt.Scan(&scopes2)
+		if scopes2 < 0 {
+			fmt.Println("Значение баллов не может быть отрицательным")
+		} else if scopes2 > limit {
+			fmt.Println("Значение баллов не может превышать", limit, "баллов")
+		} else {
+			fmt.Println("Введите результат третьего экзамена:")
+			fmt.Scan(&scopes3)
+			if scopes3 < 0 {
+				fmt.Println("Значение баллов не может быть отрицательным")
+			} else if scopes3 > limit {
+				fmt.Println("Значение баллов не может превышать", limit, "баллов")
+			} else {
+				fmt.Println("Сумма проходных баллов:", minScopes)
+
+				totalScopes := scopes1 + scopes2 + scopes3
+				fmt.Println("Количество набранных баллов:", totalScopes)
+
+				if totalScopes >= minScopes {
+					fmt.Println("Поздравляем! Вы поступили в институт.")
+				} else {
+					fmt.Println("Вы не поступили.")
+				}
+			}
+		}
+
 	}
 
 }
